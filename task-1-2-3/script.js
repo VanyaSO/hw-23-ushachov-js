08e9713dd5389cadbc68af73d022a136e4379c54
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-05-07 16:07:38 +0300

    Initial commit

---
+++ task-1-2-3/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//Task1
+console.log('//Task1');
+
+for (let a = 1; a <= 100; a++){
+    console.log(a);
+}
+
+//Task2
+console.log('//Task2');
+
+for (let a = 100; a >= 1; a--){
+    console.log(a);
+}
+
+//Task3
+console.log('//Task3');
+
+for (let a = 1; a <= 100; a++){
+    if( a % 2 === 0){
+        console.log(a);
+    }
+};
